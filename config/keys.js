66e64d197900027c8ae2646eebce2d6dfb6182ed
HuanNT <huannt@Christinass-MacBook-Pro.local>
2020-11-13 07:47:49 +0700

    First commit api

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // mongoURI : 'mongodb://admin:123456@113.161.220.166/manageYenSaoChamPa'
+    //mongoURI : 'mongodb://champaUser:champaUser@113.161.220.166:27017/manageYenSaoChamPa'
+    mongoURI : 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false'
+}
